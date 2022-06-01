8671ced488fc73bb5e841c00c6b05e37ff1428b3
Rob Lowcock <rob.lowcock@gmail.com>
2022-06-01 17:01:10 +0100

    Added config and DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,27 @@ import { AppService } from './app.service';
 import { join } from 'path';
 import { SuggestionsResolver } from './suggestions/suggestions.resolver';
 import { SuggestionsService } from './suggestions/suggestions.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+      inject: [ConfigService]
     })
   ],
   controllers: [AppController],
